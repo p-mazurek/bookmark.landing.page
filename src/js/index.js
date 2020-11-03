b3b7b3ef28a699f135c5f880c26e5258d9ce1733
Piotr Mazurek <mazurekp93@gmail.com>
2020-11-03 21:45:23 +0100

    mobile structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,36 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+console.log("HELLO 🚀");
 
-/* place your code below */
+const hamburger = document.querySelector(".hamburger--js");
+const hamburgerDeactive = document.querySelector(".hamburger-deactive--js");
+const navMobile = document.querySelector(".nav-mobile--js");
 
-console.log('HELLO 🚀')
+hamburger.addEventListener("click", () => {
+  navMobile.classList.add("head__nav-mobile--active");
+  navMobile.classList.remove("head__nav-mobile--deactive");
+});
+hamburgerDeactive.addEventListener("click", () => {
+  navMobile.classList.add("head__nav-mobile--deactive");
+  navMobile.classList.remove("head__nav-mobile--active");
+});
+
+const buttons = document.querySelectorAll(".button--js");
+const content = document.querySelectorAll(".tab");
+
+buttons.forEach((button) => {
+
+  button.addEventListener("click", function (e) {
+
+    const id = this.dataset.target;
+    const target = document.querySelector(`${id}`);
+
+    content.forEach((element) => {
+      if (element === target) {
+        element.classList.add("tab--active");
+      } else {
+        element.classList.remove("tab--active");
+      }
+    });
+  });
+});
